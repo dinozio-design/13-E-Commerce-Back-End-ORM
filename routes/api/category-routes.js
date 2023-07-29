e7cdb44afa407854effad21dcc4530e53d693d55
Sam Azimi <128939531+dinozio-design@users.noreply.github.com>
2023-07-29 10:08:31 -0400

    get by id rout works for categories

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -20,10 +20,27 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async(req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findByPk(req.params.id,{
+      include: [
+        {
+          model: Product,
+        },
+      ],
+    });
+    if(!categoryData){
+      res.status(404).json({message: "No Category found with that ID!"});
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
+
 
 router.post('/', (req, res) => {
   // create a new category
